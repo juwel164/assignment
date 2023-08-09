613324e561d6869fcfd71e9af270b8dc84d7db60
juwel164 <juwelhossain16457@gmail.com>
2023-08-09 10:01:38 +0600

    all

---
+++ assignment.js
@@ -0,0 +1,59 @@
+
+// ২. Variable কিভাবে লিখতে হয় ?
+// => We write variable as `var` in js
+Ex: var name = "hello world"
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+// => String type variable is one kind of variable which is written with "" , `` , '' etc.
+Ex: var stringType = "hello world"
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+// => Number type variable is numerical variable which is written without "" , ``, '' etc.
+Ex: var numberType = 2023;
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+// => Boolan type variable is included with only true/false. It is written without "", '', `` etc.
+Ex: var boolanType = true;
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+// => toUpperCase() & toLowerCase() is used to capitalize and smallize the texts.
+Ex: var example = "demo text";
+//For uppercase---->
+example.toUpperCase()
+console.log(toUpperCase())
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+// => There are five oparators in JavaScript.
+Ex: var num1 = 10;
+    var num2 = 20;
+var total = num1 = num2;
+var total = num1 - num2;
+var total = num1 * num2;
+var total = num1/num2;
+var total num1 % num2;
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন ।
+// => Math.abs is used for switching (-) to (+).
+Ex: var num = -29;
+var sum = Math.abs(num)
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+// => Math.ceil is used for replaceing the decimal into 1.
+Ex: var num = 0.2345343;
+var sum = Math.ceil(num)
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+// => Math.floor is used for replaceing the decimal into 0.
+Ex: var num = 0.64553;
+var sum = Math.floor(num)
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+// => Math.round is used for replacing the decimal number into full number. Greater or equal 0.5 replaced by 1. And less or equal 0.4 replaced by 0.
+Ex: var num = 0.4499999;
+var sum = Math.round(num)
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+// => March.random shows random number between 0 and 1.
+Ex: var lotery = Math.random(lotery)
+
+// asignment.js - GitHub Link 
